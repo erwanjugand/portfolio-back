10545de73d4a58bb08c70673e768de573c05b976
Erwan Jugand <erwan.jugand@gmail.com>
2019-10-04 19:26:30 +0200

    Init project

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('index.twig');
+    }
+
+}
