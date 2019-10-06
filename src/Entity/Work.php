3763c6008b268c05da6e3121bb508d05596086be
Erwan Jugand <erwan.jugand@gmail.com>
2019-10-06 13:38:09 +0200

    add url for work

--- src/Entity/Work.php
+++ src/Entity/Work.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\HasLifecycleCallbacks()
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\WorkRepository")
  */
@@ -23,6 +24,11 @@ class Work
      */
     private $title;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $url;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -38,5 +44,37 @@ class Work
         $this->title = $title;
 
         return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function CreatedWork()
+    {
+        $url = str_replace("'", '-',str_replace('"', '',wordwrap(strtolower($this->title), 1, '-', 0)));
+        $url = strtr(utf8_decode($url), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+        $this->url = $url;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function UpdatedWork()
+    {
+        $url = str_replace("'", '-',str_replace('"', '',wordwrap(strtolower($this->title), 1, '-', 0)));
+        $url = strtr(utf8_decode($url), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+        $this->url = $url;
     }
 }
